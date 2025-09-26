d3e0ff4325946a392550f9b9eaf960fe625e95a7
Awa Precious <150751312+AwaPreciousNkeng@users.noreply.github.com>
2025-09-26 05:05:50 +0100

    Completed

--- src/lib.rs
+++ src/lib.rs
@@ -1,148 +1,179 @@
-use std::{env, io};
-use std::fs::OpenOptions;
-use std::io::{BufReader, BufWriter, Read, Write};
-use std::path::Path;
-use colored::Colorize;
+use std::process;
+use std::sync::atomic;
+use std::sync::atomic::AtomicU64;
 
-pub struct Entry {
-    pub todo_entry: String,
-    pub done: bool
+#[derive(Debug)]
+pub struct Task {
+    pub task: String,
+    pub is_completed: bool,
+    pub id: u64
 }
 
-impl Entry {
-    pub fn new(todo_entry: String, done: bool) -> Self {
-        Self {
-            todo_entry,
-            done
-        }
+impl Task {
+    fn update_status(&mut self) {
+        self.is_completed = true;
     }
 
-    pub fn file_line(&self) -> String {
-        let symbol = if self.done { "[*] " } else { "[ ]" };
-        format!("{}{}\n", symbol, self.todo_entry)
-    }
-
-    pub fn list_line(&self, number: usize) -> String {
-        //Checks if the current task is completed or not...
-        let todo_entry = if self.done {
-            //DONE
-            //If the task is completed, then it prints it as it is
-            self.todo_entry.strikethrough().to_string()
-        } else {
-            //Not Done
-            //If the task is not completed yet, then it prints it with a strikethrough
-            self.todo_entry.clone()
-        };
-        format!("{} {}\n", number, self.todo_entry)
-    }
-
-    pub fn read_line(line: &String) -> Self {
-        let done = &line[..4] == "[*] ";
-        let todo_entry = (&line[4..]).to_string();
-        Self {
-            todo_entry,
-            done
-        }
-    }
-
-    pub fn raw_line(&self) -> String {
-        format!("{}\n", self.todo_entry)
+    fn update_task(&mut self, new_name: &str) {
+        self.task = new_name.to_string();
     }
 }
 
-pub struct Todo {
-    pub todo: Vec<String>,
-    pub todo_path: String,
-    pub todo_bak: String,
-    pub no_backup: bool
+static UNIQUE_ID: AtomicU64 = AtomicU64::new(1);
+
+fn display_todo (todo_list: &Vec<Task>) {
+    if todo_list.len() == 0 {
+        println!("Empty Todo list");
+        return;
+    }
+
+    for task in todo_list {
+        println!("id: {}, name: {}, completed: {}", task.id, task.task, task.is_completed);
+    }
 }
 
-impl Todo {
-    pub fn new() -> Result<Self, String> {
-        let todo_path: String = match env::var("TODO_PATH") {
-            Ok(t) => t,
-            Err(_) => {
-                let home = env::var("HOME").unwrap();
+fn add_new_task(todo_list: &mut Vec<Task>, task_name: &str) {
+    let id = UNIQUE_ID.fetch_add(1, atomic::Ordering::SeqCst);
 
-                //Look for a legacy TODO file path
-                let legacy_todo = format!("{}/TODO", &home);
-                match Path::new(&legacy_todo).exists() {
-                    true => legacy_todo,
-                    false => format!("{}/.todo", &home)
+    let task: Task = Task {
+        task: task_name.to_string(),
+        is_completed: false,
+        id
+    };
+    todo_list.push(task);
+    println!("{} added to the todo list", task_name);
+}
+
+fn remove_task(todo_list: &mut Vec<Task>, id: u64) {
+    todo_list.retain(|t| t.id != id);
+}
+
+fn get_task(todo_list: &mut Vec<Task>, id: u64) -> Option<&mut Task> {
+    todo_list.iter_mut().find(|task| task.id == id)
+    }
+
+fn display_help() {
+    let help: &str = "
+        Welcome to the todo_list application.
+        structure of query:
+            command [arguments]
+
+        supported commands:
+            add - Add a new task to the todo list, followed by a new task string. The task string should NOT be space separated.
+
+                usage: >add task_string
+
+            show - Display the todo list
+
+                usage: >show
+
+            delete - delete a task from the todo list, based on the task id provided by the user in the prompt.
+
+                usage: >delete task_id
+
+            update - change the name of a task, followed by an integer number task id.
+
+                usage: >update task_id new_task_string
+
+            done - change the done status of a task from false to true, follwed by an integer number task id.
+
+                usage: >done task_id
+
+            exit- exit the program.
+
+                usage: >exit
+
+            help - display this help message.
+
+                usage: >help
+
+        arguments:
+            task_id: the unique id assigned to each task.
+
+            task_string: the string for the task provided by the user. ";
+
+    println!("{}", help);
+}
+
+fn parse_args(args: Vec<&str>, todo_list: &mut Vec<Task>) {
+    if args.is_empty() {
+        println!("No commands provided");
+    }
+    let command = args[0];
+
+    match command {
+        "add" => {
+            if let Some(value) = args.get(1) {
+                let new_task = *value;
+                add_new_task(todo_list, new_task);
+                display_todo(todo_list);
+            } else {
+                println!("Please specify a task name");
+            }
+        },
+        "show" => {
+            display_todo(todo_list);
+        },
+        "delete" => {
+            if let Some(id_str) = args.get(1) {
+                match id_str.parse::<u64>() {
+                    Ok(id) => {
+                        remove_task(todo_list, id);
+                    }
+
+                    Err(message) => {
+                        println!("Invalid Id: {}", message);
+                    }
                 }
             }
-        };
+        },
 
-        let todo_bak: String = match env::var("TODO_BAK_DIR") {
-            Ok(t) => t,
-            Err(_) => String::from("/tmp/todo.bak")
-        };
-
-        let no_backup = env::var("TODO_NOBACKUP").is_ok();
-        let todofile = OpenOptions::new()
-            .write(true)
-            .read(true)
-            .create(true)
-            .open(&todo_path)
-            .expect("failed to open todo file");
-
-        //Creates a new buf reader
-        let mut buf_reader = BufReader::new(&todofile);
-
-        //Empty String ready to be filled with TODOs
-        let mut contents = String::new();
-
-        //Loads "contents" String with data
-        buf_reader.read_to_string(&mut contents).unwrap();
-
-        //Splits contents of the TODO file into a todo vector
-        let todo = contents.lines().map(str::to_string).collect();
-
-        //Returns todo
-        Ok(Self {
-            todo,
-            todo_path,
-            todo_bak,
-            no_backup
-        })
-    }
-
-    //Prints every todo saved
-    pub fn list(&self) {
-        let stdout = io::stdout();
-        //Buffered writer for stdout stream
-        let mut writer = BufWriter::new(stdout);
-        let mut data = String::new();
-        //This loop will repeat itself for each task in TODO file
-        for (number, task) in self.todo.iter().enumerate() {
-            let entry = Entry::read_line(task);
-            let number = number + 1;
-            let line = entry.list_line(number);
-            data.push_str(&line);
-        }
-        writer
-            .write_all(data.as_bytes())
-            .expect("failed to write to stdout");
-    }
-
-    pub fn raw(&self, arg: &[String]) {
-        if arg.len() > 1 {
-            eprintln!("Todo raw takes only 1 argument, not {}", arg.len());
-        } else if arg.is_empty() {
-            eprintln!("Todo raw takes one argument (done/todo");
-        } else {
-            let stdout = io::stdout();
-            //Buffered writer for stdout stream
-            let mut writer = BufWriter::new(stdout);
-            let mut data = String::new();
-            let arg = &arg[0];
-            //This loop will repeat itself for each task in todo file
-            for task in self.todo.iter() {
-                let entry = Entry::read_line(task);
-                if entry.done && arg == "done" {
-
+        "update" => {
+            if let Some(id_str) = args.get(1) {
+                match id_str.parse::<u64>() {
+                    Ok(id) => {
+                        if let Some(task) = get_task(todo_list, id) {
+                            if let Some(value) = args.get(2) {
+                                task.update_task(value);
+                                println!("Updated Task: {}", id);
+                            } else {
+                                println!("No new task provided");
+                            }
+                        } else {
+                            println!("Task not found in todo list");
+                        }
+                    },
+                    Err(message) => {
+                        println!("Invalid Id: {}", message);
+                    }
                 }
             }
         }
+        "done" => {
+            if let Some(id_str) = args.get(1) {
+                match id_str.parse::<u64>() {
+                    Ok(id) => {
+                        if let Some(task) = get_task(todo_list, id) {
+                            task.update_status();
+                        } else {
+                            println!("Task Id not found in the list");
+                        }
+                    },
+                    Err(message) => {
+                        println!("Invalid Id: {}", message);
+                    }
+                }
+            }
+        }
+        "exit" => {
+            process::exit(0);
+        }
+        "help" |_ => {
+            display_help();
+        }
     }
+}
+
+pub fn run(args: Vec<&str>, todo_list: &mut Vec<Task>) {
+    parse_args(args, todo_list);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, stdout, Write};
+use todo::Task;
+
+fn run_prompt(todo: &mut Vec<Task>) {
+    loop {
+        let mut stdout = stdout().lock();
+        print!("(PCodes-List) > ");
+        stdout.flush().expect("Could not flush stdout");
+
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).expect("Could not read line");
+
+        //Take the args into the run function of lib and get the result of the computation out.
+        let command: &str = buffer.split_whitespace().collect::<Vec<&str>>()[0];
+        let task_string = concatenate_task_string(buffer.split_whitespace().collect());
+        let args = vec![command, task_string.as_str()];
+        todo::run(args, todo);
+    }
 }
+
+fn concatenate_task_string(word_vec: Vec<&str>) -> String {
+    let mut result = String::new();
+    // We are assuming that everything after the first word is part of task string.
+    for i in 1..word_vec.len() {
+        result = result + word_vec[i];
+        result = result + " ";
+    }
+    result
+}
+
+fn main() {
+    let mut todo: Vec<Task> = Vec::new();
+    run_prompt(&mut todo);
+}
+
+
